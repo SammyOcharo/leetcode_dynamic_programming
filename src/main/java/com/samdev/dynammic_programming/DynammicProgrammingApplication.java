506f4c7a93d2ffcd0b6306a23ddc95d14e0e80c9
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-07-10 11:58:11 +0300

    dynamic programming initialisation

---
+++ src/main/java/com/samdev/dynammic_programming/DynammicProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.samdev.dynammic_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynammicProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynammicProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samdev/dynammic_programming/DynammicProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samdev.dynammic_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynammicProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
