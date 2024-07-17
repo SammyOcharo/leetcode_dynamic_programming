db060991e8292df603cfb2619fd9245ec6d0b444
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-07-17 08:20:58 +0300

    maxNonDecreasingLength JUnits

--- src/test/java/com/samdev/dynammic_programming/DecreasingLengthTest.java
+++ src/test/java/com/samdev/dynammic_programming/DecreasingLengthTest.java
@@ -9,7 +9,11 @@ class DecreasingLengthTest {
     @Test
     void maxNonDecreasingLength() {
         int[] nums1 = {5,2,2};
+        int[] nums2 = {1, 2, 3, 4};
+        int[] nums3 = {4, 3, 2, 6};
 
         assertEquals(2, DecreasingLength.maxNonDecreasingLength(nums1));
+        assertEquals(4, DecreasingLength.maxNonDecreasingLength(nums2));
+        assertEquals(2, DecreasingLength.maxNonDecreasingLength(nums3));
     }
 }
